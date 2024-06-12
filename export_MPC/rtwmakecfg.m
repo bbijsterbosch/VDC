2f7a539a2459d0e6fa1b827be325ed594e1c6272
Bas <basbijsterbosch97@gmail.com>
2024-06-12 21:26:51 +0200

    added simulink model

--- export_MPC/rtwmakecfg.m
+++ export_MPC/rtwmakecfg.m
@@ -2,7 +2,7 @@ function makeInfo = rtwmakecfg()
 %RTWMAKECFG adds include and source directories to the make files.
 %    makeInfo=
 %    Simulink version    : 23.2 (R2023b) 01-Aug-2023
-%    MATLAB file generated on : 10-Jun-2024 17:30:29
+%    MATLAB file generated on : 12-Jun-2024 20:28:44
 
 % Verify the Simulink version
 verify_simulink_version();
